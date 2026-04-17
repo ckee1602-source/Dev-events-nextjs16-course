c713b4f0a51417092e088dfcc0f766d434798ccb
ckee1602-source <ckee1602@gmail.com>
2026-04-17 18:46:45 +0530

    fix: remove next() from async pre-save hook

---
+++ posthog.js
@@ -0,0 +1,11 @@
+'use client'
+
+import posthog from 'posthog-js'
+
+export default function CheckoutPage() {
+    function handlePurchase() {
+        posthog.capture('purchase_completed', { amount: 99 })
+    }
+
+    return <button onClick={handlePurchase}>Complete purchase</button>
+}
